616da486c05cd0ddd25c141f93d9f05e54fddd2d
Sofus <sofusboelskifte91@gmail.com>
2026-03-01 13:17:26 +0100

    Velkomst og login

--- Program.cs
+++ Program.cs
@@ -4,7 +4,70 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string workerID = "";
+            string name = "";
+            bool isValidLogin = false;
+
+            while (!isValidLogin)
+            {
+                Console.Clear();
+                Console.WriteLine("=========================");
+                Console.WriteLine("KONTOR NORD BOOKINGSYSTEM");
+                Console.WriteLine("=========================");
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                Console.WriteLine("Indtast dit medarbejder ID for at begynde: ");
+                workerID = Console.ReadLine();
+
+
+
+                if (workerID == "SOM")
+                {
+                    name = "Sofie Møller";
+                    isValidLogin = true;
+                }
+                else if (workerID == "AMR")
+                {
+                    name = "Amir Rahimi";
+                    isValidLogin = true;
+                }
+                else if (workerID == "JOT")
+                {
+                    name = "Jonas Tved";
+                    isValidLogin = true;
+                }
+                else if (workerID == "LOF")
+                {
+                    name = "Louise Falk";
+                    isValidLogin = true;
+                }
+                else if (workerID == "MEA")
+                {
+                    name = "Mette Ates";
+                    isValidLogin = true;
+                }
+                else if (workerID == "HEK")
+                {
+                    name = "Henrik Krøll";
+                    isValidLogin = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Ugyldigt medarbejder ID!");
+                    Console.WriteLine("Tryk på en tast for at prøve igen...");
+                    Console.ReadLine(); 
+                }
+            }
+           
+            Console.Clear();
+            Console.WriteLine("=========================");
+            Console.WriteLine($"VELKOMMEN {name}");
+            Console.WriteLine("=========================");
+            Console.WriteLine("");
+            Console.WriteLine("Tryk på en tast for at komme ind i menuen");
+            Console.ReadLine();
         }
     }
 }
